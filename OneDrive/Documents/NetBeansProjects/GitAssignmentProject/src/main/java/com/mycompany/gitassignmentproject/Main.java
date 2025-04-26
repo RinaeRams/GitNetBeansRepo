ec3f1200519228de2a2056525ac8e7dc672d3c1a
Rinae Innocent Ramadi <222001313@ump.ac.za>
2025-04-26 12:05:32 +0200

    Committing work

--- OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/main/java/com/mycompany/gitassignmentproject/Main.java
+++ OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/main/java/com/mycompany/gitassignmentproject/Main.java
@@ -8,6 +8,9 @@ package com.mycompany.gitassignmentproject;
  *
  * @author innoc
  */
-public class Main {
-    
+  public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
 }
+
