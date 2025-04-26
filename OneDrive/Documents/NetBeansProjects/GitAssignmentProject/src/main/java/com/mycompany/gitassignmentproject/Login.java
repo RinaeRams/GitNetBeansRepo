62af02c42314c96360b601afda94e5d725df4d10
Rinae Innocent Ramadi <222001313@ump.ac.za>
2025-04-26 13:07:17 +0200

    Added Login.java on feature-login branch

---
+++ OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/main/java/com/mycompany/gitassignmentproject/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gitassignmentproject;
+
+/**
+ *
+ * @author innoc
+ */
+  public class Login {
+    public static void main(String[] args) {
+        System.out.println("This is the login feature!");
+    }
+}
+
